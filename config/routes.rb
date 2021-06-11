829c3ff38fb89ec46db4cf5704bad87d28cade4a
Paul Fell <swellpf@gmail.com>
2021-06-11 12:20:04 -0400

    namespace routes and move controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :watchlist_movies
-  resources :users
-  resources :watchlists
-  resources :genres
-  resources :movies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :comments
+      resources :watchlist_movies
+      resources :users
+      resources :watchlists
+      resources :genres
+      resources :movies
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_155932) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
