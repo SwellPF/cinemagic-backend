91cedaeeea675584b3b7127adb328e3beb86a8e4
Paul Fell <swellpf@gmail.com>
2021-06-14 19:43:09 -0400

    Adjusted MovieSerializer for genre.name

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,6 +1,13 @@
 class MovieSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :title, :description, :genre_id, :year_released, :image_link
+  attribute :genre_name do |movie|
+    movie.genre.name 
+  end
 
   belongs_to :genre
+  has_many :comments
+
+  
+  
 end
